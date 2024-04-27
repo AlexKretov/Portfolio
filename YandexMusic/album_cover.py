8c4c19e98d7dc61bdb693b83e96c1fc352a20834
Александр Кретов <128688742+AlexKretov@users.noreply.github.com>
2024-04-27 13:01:09 +0300

    Update album_cover.py

--- YandexMusic/album_cover.py
+++ YandexMusic/album_cover.py
@@ -47,9 +47,6 @@ def get_single_image_embedding(my_image):
     embedding_as_np = embedding.cpu().detach().numpy()
     return embedding_as_np
 
-@st.cache_resource
-def get_single_image_embedding_session(my_image):
-    return get_single_image_embedding(my_image)
 	
 # Функция принимает на вход вектор изображения vec, в формета ячейки датафрейма, в которой лежит эмбеддинг, и число  ответов n,а возвращает жанр наиболее часто встречающийся
 # среди n обложек альбомов наиболее близких к заданному вектору vec
@@ -62,17 +59,14 @@ def find_neighbor(vec,n):
         rez.loc[ len(rez.index )] = [df.iloc[int(idx)]['genre']]
     return rez.value_counts(normalize=True).head(1).index[0][0]
 
-@st.cache_resource
-def find_neighbor_session(vec,n):
-    return find_neighbor(vec,n)
 
 df = pd.read_csv('YandexMusic/covers.csv')
 try:
 	image_data = uploaded_file.getvalue()
 	img = Image.open(BytesIO(image_data))
-	vector = get_single_image_embedding_session(img)
+	vector = get_single_image_embedding(img)
 	
-	genre = find_neighbor_session(vector, 7)
+	genre = find_neighbor(vector, 7)
 	cap = "Вероятный жанр: " +  str(genre)
 except: 
 	st.write('Ожидаем загрузку обложки')
