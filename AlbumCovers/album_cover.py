ddcf37625a4ff757f6e55829362f10ca23db342b
Александр Кретов <128688742+AlexKretov@users.noreply.github.com>
2024-04-27 16:33:46 +0300

    Update album_cover.py

--- AlbumCovers/album_cover.py
+++ AlbumCovers/album_cover.py
@@ -1,4 +1,6 @@
+
 import streamlit as st
+#from streamlit import caching
 from faiss import write_index, read_index
 from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
 import numpy as np
@@ -6,7 +8,8 @@ import pandas as pd
 import torch
 from PIL import Image
 from io import BytesIO
-index = read_index("large.index")
+index = read_index("YandexMusic/large.index")
+
 
 #Функция загружет выбранную версию CLIP на имеющийся девайс, на вход получает идентификатор модели согласно каталогу 
 def get_model_info(model_ID, device):
@@ -18,15 +21,19 @@ def get_model_info(model_ID, device):
 	tokenizer = CLIPTokenizer.from_pretrained(model_ID)
        # Return model, processor & tokenizer
 	return model, processor, tokenizer
-	
+@st.cache_resource
+def get_model_info_session(model_ID, device):
+    return get_model_info(model_ID, device)
 # Set the device
 device = "cuda" if torch.cuda.is_available() else "cpu"
+#device = "cpu"
 
 
 # Define the model ID
 model_ID = "openai/clip-vit-base-patch32"
 # Get model, processor & tokenizer
-model, processor, tokenizer = get_model_info(model_ID, device)
+model, processor, tokenizer = get_model_info_session(model_ID, device)
+#st.cache.clear()
 st.title('Определение жанра альбома по картинке')
 uploaded_file = st.file_uploader("Загрузите обложку альбома",type=['png','jpg','bmp','tif'])
 # Функция принимает на вход картинку, а возвращает эмбеддинг
@@ -40,6 +47,8 @@ def get_single_image_embedding(my_image):
     # convert the embeddings to numpy array
     embedding_as_np = embedding.cpu().detach().numpy()
     return embedding_as_np
+
+	
 # Функция принимает на вход вектор изображения vec, в формета ячейки датафрейма, в которой лежит эмбеддинг, и число  ответов n,а возвращает жанр наиболее часто встречающийся
 # среди n обложек альбомов наиболее близких к заданному вектору vec
 def find_neighbor(vec,n):
@@ -50,23 +59,23 @@ def find_neighbor(vec,n):
     for idx in I.flatten():
         rez.loc[ len(rez.index )] = [df.iloc[int(idx)]['genre']]
     return rez.value_counts(normalize=True).head(1).index[0][0]
+
+
+df = pd.read_csv('YandexMusic/covers.csv')
 try:
-    image_data = uploaded_file.getvalue()
-    img = Image.open(BytesIO(image_data))
-    vector = get_single_image_embedding(img)
-    df = pd.read_csv('covers.csv')
-    genre = find_neighbor(vector, 7)
-
-    cap = "Вероятный жанр: " +  str(genre)
-except:
-     st.write('Ожидаем загруки обложку')
-
-
-
+	image_data = uploaded_file.getvalue()
+	img = Image.open(BytesIO(image_data))
+	vector = get_single_image_embedding(img)
+	
+	genre = find_neighbor(vector, 7)
+	cap = "Вероятный жанр: " +  str(genre)
+except: 
+	st.write('Ожидаем загрузку обложки')
 try:
      st.image(image_data, caption=cap, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+     #st.cache.clear()
 except:
-     st.write('Ожидаем загруки обложку')
+     st.write('. . .')
 
 #rec_list=[]
 #for i in rage(len(idx)):
